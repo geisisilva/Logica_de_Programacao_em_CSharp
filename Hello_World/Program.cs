b03b1dcbb5cadf090dee5c3255ae34e4fa10709f
Geisilane S. <dasilvacunhageisilane9@gmail.com>
2024-10-05 16:41:06 -0400

    Meu primeiro código em CSharp

---
+++ Hello_World/Program.cs
@@ -0,0 +1,37 @@
+﻿// Na criação de um "algoritmo", podemos seguir este sete passos ↴
+// 1.NOME, 2.INÍCIO, 3.DADOS/VARIÁVEIS, 4.ENTRADA, 5.PROCESSAMENTO, 6.SAÍDA, 7.FIM
+
+// 1.NOME:
+namespace Calculadora_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.INÍCIO
+            // Faça um algorítimo que receba dois valores e mostre
+            // a soma desses dois valores.
+
+            // 3.DADOS/VARIÁVEIS
+            double n1 = 0;
+            double n2 = 0;
+            double soma = 0;
+
+            // 4.ENTRADA
+            Console.WriteLine("Digite o primeiro número:");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("\r\nDigite o segundo número:");
+            n2 = double.Parse(Console.ReadLine());
+
+            // 5.PROCESSAMENTO
+            soma = n1 + n2;
+
+            // 6.SAÍDA
+            Console.WriteLine("\r\nOs números somados são: " + soma);
+            Console.ReadKey();
+
+            Console.WriteLine("\r\nHello World!");
+        }
+    }
+}
+// 7.FIM :)
